5c50304f0b43901afb2aa23a5962c856a65e969c
VeryEpicKebap <164046592+VeryEpicKebap@users.noreply.github.com>
2025-07-07 23:34:47 +0300

    Create nova.py

---
+++ nova.py
@@ -0,0 +1,23 @@
+import sys, os,time
+components_path = os.path.join(os.getcwd(), "COMPONENTS")
+col = os.get_terminal_size().columns
+lin = os.get_terminal_size().lines
+if os.path.exists(components_path):
+    sys.path.insert(0, components_path)
+    try:
+        print("[ init ] novascr")
+        import novascr
+    except ImportError:
+        print("Failure importing novascr")
+        sys.exit()
+
+    try:
+        print("[ init ] novaio")
+        import novaio
+    except ImportError:
+        print("Failure importing novaio")
+        sys.exit()
+else:
+    print(f"Components folder not found: {components_path}")
+    sys.exit()
+
